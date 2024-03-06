c6752a3f20b7ffafc59dc53b98be49868b296e0a
Alex Tompkins <alex.tompkins@adyen.com>
2024-03-06 13:25:27 +0100

    move manifest instance to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
 import { mountServiceWorker } from './mountServiceWorker';
-import { manifest } from './manifest';
+import { Manifest } from './manifest';
+
+const manifest: Manifest = {
+  url: 'http://localhost:5173/cdn',
+  version: '2.1.3',
+  assets: [
+    {
+      filename: 'doThing.js',
+      sri: 'sha256-hash123',
+      entrypoint: true,
+    },
+    {
+      filename: 'doOtherThing.js',
+      sri: 'sha256-/rQ5rYwYg09wG/jnlsC8TtZW1hzpccgh7JnpaDMAF/M=',
+      entrypoint: true,
+    },
+    {
+      filename: 'subModule.js',
+      sri: 'sha256-hpSs81nJ1MUCXLSokfxT+TTmxodz9LPyHX7ssF02W2Y=',
+    },
+  ],
+};
 
 window.addEventListener('load', async () => {
   try {
--- src/manifest.ts
+++ src/manifest.ts
@@ -9,24 +9,3 @@ export interface Manifest {
   version: string;
   assets: CdnAssetMeta[];
 }
-
-export const manifest: Manifest = {
-  url: 'http://localhost:5173/cdn',
-  version: '2.1.3',
-  assets: [
-    {
-      filename: 'doThing.js',
-      sri: 'sha256-hash123',
-      entrypoint: true,
-    },
-    {
-      filename: 'doOtherThing.js',
-      sri: 'sha256-/rQ5rYwYg09wG/jnlsC8TtZW1hzpccgh7JnpaDMAF/M=',
-      entrypoint: true,
-    },
-    {
-      filename: 'subModule.js',
-      sri: 'sha256-hpSs81nJ1MUCXLSokfxT+TTmxodz9LPyHX7ssF02W2Y=',
-    },
-  ],
-};
